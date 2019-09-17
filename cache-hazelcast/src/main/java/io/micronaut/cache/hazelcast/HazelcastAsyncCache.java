ce1a20c5964c6c8876b73655f878bbecabcc4969
niravassar <assarn@objectcomputing.com>
2019-09-17 09:37:59 -0500

    async implementation

--- cache-hazelcast/src/main/java/io/micronaut/cache/hazelcast/HazelcastAsyncCache.java
+++ cache-hazelcast/src/main/java/io/micronaut/cache/hazelcast/HazelcastAsyncCache.java
@@ -1,7 +1,6 @@
 package io.micronaut.cache.hazelcast;
 
 import com.hazelcast.core.ExecutionCallback;
-import com.hazelcast.core.ICompletableFuture;
 import com.hazelcast.core.IMap;
 import io.micronaut.cache.AsyncCache;
 import io.micronaut.core.convert.ConversionContext;
@@ -47,43 +46,83 @@ public class HazelcastAsyncCache implements AsyncCache<IMap<Object, Object>> {
 
     @Override
     public <T> CompletableFuture<T> get(@Nonnull Object key, @Nonnull Argument<T> requiredType, @Nonnull Supplier<T> supplier) {
-//        ArgumentUtils.requireNonNull("key", key);
-//        return get(key, requiredType).orElseGet(supplier);
-        return null;
+        ArgumentUtils.requireNonNull("key", key);
+        CompletableFuture<T> future = new CompletableFuture<>();
+        nativeCache.getAsync(key).andThen(new ExecutionCallback<Object>() {
+            @Override
+            public void onResponse(Object response) {
+                if (response != null) {
+                    future.complete(conversionService.convert(response, ConversionContext.of(requiredType)).get());
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                future.complete(supplier.get());
+            }
+        });
+        return future;
     }
 
     @SuppressWarnings("unchecked")
     @Nonnull
     @Override
     public <T> CompletableFuture<Optional<T>> putIfAbsent(@Nonnull Object key, @Nonnull T value) {
-//        ArgumentUtils.requireNonNull("key", key);
-//        ArgumentUtils.requireNonNull("value", value);
-//        final T v = (T) nativeCache.putIfAbsent(key, value);
-//        final Class<T> aClass = (Class<T>) value.getClass();
-//        return conversionService.convert(v, aClass);
-        return null;
+        ArgumentUtils.requireNonNull("key", key);
+        ArgumentUtils.requireNonNull("value", value);
+        CompletableFuture<Optional<T>> future = new CompletableFuture<>();
+        final T v = (T) nativeCache.putIfAbsent(key, value);
+        final Class<T> aClass = (Class<T>) value.getClass();
+        future.complete(conversionService.convert(v, aClass));
+        return future;
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public CompletableFuture<Boolean> put(@Nonnull Object key, @Nonnull Object value) {
-//        ArgumentUtils.requireNonNull("key", key);
-//        ArgumentUtils.requireNonNull("value", value);
-//        nativeCache.put(key, value);
-        return CompletableFuture.completedFuture(true);
+        ArgumentUtils.requireNonNull("key", key);
+        ArgumentUtils.requireNonNull("value", value);
+        CompletableFuture<Boolean> future = new CompletableFuture<>();
+        nativeCache.putAsync(key, value).andThen(new ExecutionCallback<Object>() {
+            @Override
+            public void onResponse(Object response) {
+                if (response != null) {
+                    future.complete(true);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                future.completeExceptionally(t);
+            }
+        });
+        return future;
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public CompletableFuture<Boolean> invalidate(@Nonnull Object key) {
         ArgumentUtils.requireNonNull("key", key);
-        nativeCache.remove(key);
-        return CompletableFuture.completedFuture(true);
+        CompletableFuture<Boolean> future = new CompletableFuture<>();
+        nativeCache.removeAsync(key).andThen(new ExecutionCallback<Object>() {
+            @Override
+            public void onResponse(Object response) {
+                if (response != null) {
+                    future.complete(true);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                future.completeExceptionally(t);
+            }
+        });
+        return future;
     }
 
     @Override
     public CompletableFuture<Boolean> invalidateAll() {
-        //nativeCache.clear();
+        nativeCache.clear();
         return CompletableFuture.completedFuture(true);
     }
 
