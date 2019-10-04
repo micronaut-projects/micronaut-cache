24dbf7fd1c750b85edec39dcc50d3114e11efecf
Davide Cocco <davide.cocco@oracle.com>
2019-10-04 17:20:12 +0200

    First cut of tiering options

--- cache-ehcache/src/test/groovy/io/micronaut/cache/ehcache/EhcacheCacheManagerSpec.groovy
+++ cache-ehcache/src/test/groovy/io/micronaut/cache/ehcache/EhcacheCacheManagerSpec.groovy
@@ -2,13 +2,18 @@ package io.micronaut.cache.ehcache
 
 import io.micronaut.cache.SyncCache
 import io.micronaut.context.ApplicationContext
+import org.ehcache.config.units.EntryUnit
+import org.ehcache.config.units.MemoryUnit
+import org.ehcache.core.EhcacheManager
 import spock.lang.Specification
+
+import static org.ehcache.config.ResourceType.Core.HEAP
 
 class EhcacheCacheManagerSpec extends Specification {
 
     void "it create caches from configuration"() {
         given:
-        ApplicationContext ctx = ApplicationContext.run(ApplicationContext, [
+        ApplicationContext ctx = ApplicationContext.run([
                 "ehcache.caches.foo.keyType": "java.lang.Long",
                 "ehcache.caches.foo.valueType": "java.lang.String"
         ])
@@ -23,7 +28,37 @@ class EhcacheCacheManagerSpec extends Specification {
         SyncCache cache = ehcacheCacheManager.getCache('foo')
         cache.nativeCache.runtimeConfiguration.keyType == Long
         cache.nativeCache.runtimeConfiguration.valueType == String
-        cache.nativeCache
+        cache.nativeCache.runtimeConfiguration.resourcePools.pools.size() == 1
+        cache.nativeCache.runtimeConfiguration.resourcePools.pools[HEAP].size == 10
+    }
+
+    void "it can create entries-based heap tiered cache"() {
+        given:
+        ApplicationContext ctx = ApplicationContext.run([
+                "ehcache.caches.foo.heap.max-entries": 27
+        ])
+        EhcacheManager ehcacheManager = ctx.getBean(EhcacheManager)
+
+        expect:
+        SyncCache cache = ehcacheManager.getCache('foo')
+        cache.nativeCache.runtimeConfiguration.resourcePools.pools.size() == 1
+        cache.nativeCache.runtimeConfiguration.resourcePools.pools[HEAP].unit == EntryUnit.ENTRIES
+        cache.nativeCache.runtimeConfiguration.resourcePools.pools[HEAP].size == 27
+    }
+
+    void "it can create size-based heap tiered cache"() {
+        given:
+        ApplicationContext ctx = ApplicationContext.run([
+                "ehcache.caches.foo.heap.max-size": '15Mb'
+        ])
+
+        EhcacheManager ehcacheManager = ctx.getBean(EhcacheManager)
+
+        expect:
+        SyncCache cache = ehcacheManager.getCache('foo')
+        cache.nativeCache.runtimeConfiguration.resourcePools.pools.size() == 1
+        cache.nativeCache.runtimeConfiguration.resourcePools.pools[HEAP].unit == MemoryUnit.B
+        cache.nativeCache.runtimeConfiguration.resourcePools.pools[HEAP].size == 15 * 1024 * 1024
     }
 
 }
