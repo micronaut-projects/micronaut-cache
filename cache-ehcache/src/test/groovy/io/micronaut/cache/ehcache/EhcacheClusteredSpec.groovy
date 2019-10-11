8fb110b9c620aa44e5a991d7659d4a65532ef568
Davide Cocco <davide.cocco@oracle.com>
2019-10-11 13:21:14 +0200

    Basic cluster support

---
+++ cache-ehcache/src/test/groovy/io/micronaut/cache/ehcache/EhcacheClusteredSpec.groovy
@@ -0,0 +1,29 @@
+package io.micronaut.cache.ehcache
+
+import io.micronaut.cache.SyncCache
+import io.micronaut.context.ApplicationContext
+import org.ehcache.clustered.client.config.ClusteringServiceConfiguration
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.spock.Testcontainers
+import spock.lang.Specification
+
+@Testcontainers
+class EhcacheClusteredSpec extends Specification {
+
+    public GenericContainer terracotta = new GenericContainer("terracotta/terracotta-server-oss:5.6.4")
+            .withExposedPorts(9410)
+
+    void "it can create a clustered cache"() {
+        given:
+        ApplicationContext ctx = ApplicationContext.run([
+                "ehcache.cluster.uri": "terracotta://localhost:${terracotta.firstMappedPort}/my-application",
+                "ehcache.caches.foo.enabled": true
+        ])
+        EhcacheCacheManager ehcacheManager = ctx.getBean(EhcacheCacheManager)
+        SyncCache cache = ehcacheManager.getCache('foo')
+
+        expect:
+        ehcacheManager.cacheManager.runtimeConfiguration.services[0] instanceof ClusteringServiceConfiguration
+        ehcacheManager.cacheManager.runtimeConfiguration.services[0].connectionSource.clusterUri == URI.create("terracotta://localhost:${terracotta.firstMappedPort}/my-application")
+    }
+}
