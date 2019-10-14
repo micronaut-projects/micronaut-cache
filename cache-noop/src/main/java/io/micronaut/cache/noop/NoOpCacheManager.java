9ce59147f682960de9b3a6b2fa0ae3aec78cdd16
Marcel Overdijk <marceloverdijk@gmail.com>
2019-10-14 19:15:56 +0100

    #12 Added NoOp cache implementation

---
+++ cache-noop/src/main/java/io/micronaut/cache/noop/NoOpCacheManager.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.cache.noop;
+
+import io.micronaut.cache.DefaultCacheManager;
+import io.micronaut.cache.SyncCache;
+import io.micronaut.context.annotation.Primary;
+import io.micronaut.context.annotation.Replaces;
+
+import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A no operation {@link io.micronaut.cache.CacheManager} implementation suitable for disabling caching.
+ *
+ * @author Marcel Overdijk
+ * @since 1.0.0
+ */
+@Replaces(DefaultCacheManager.class)
+@Primary
+public class NoOpCacheManager implements io.micronaut.cache.CacheManager<Object> {
+
+    private Map<String, NoOpSyncCache> cacheMap;
+
+    /**
+     * Constructor.
+     */
+    public NoOpCacheManager() {
+        this.cacheMap = new HashMap<>();
+    }
+
+    @Nonnull
+    @Override
+    public Set<String> getCacheNames() {
+        return cacheMap.keySet();
+    }
+
+    @Nonnull
+    @Override
+    public SyncCache<Object> getCache(String name) {
+        NoOpSyncCache syncCache = this.cacheMap.get(name);
+        if (syncCache == null) {
+            syncCache = new NoOpSyncCache(name);
+            this.cacheMap.put(name, syncCache);
+        }
+        return syncCache;
+    }
+}
---
+++ cache-noop/src/main/java/io/micronaut/cache/noop/NoOpSyncCache.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.cache.noop;
+
+import io.micronaut.cache.SyncCache;
+import io.micronaut.core.type.Argument;
+
+import javax.annotation.Nonnull;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+/**
+ * A no operation {@link SyncCache} implementation suitable for disabling caching.
+ *
+ * @author Marcel Overdijk
+ * @since 1.0.0
+ */
+public class NoOpSyncCache implements SyncCache<Object> {
+
+    private String name;
+
+    /**
+     * Constructor.
+     */
+    public NoOpSyncCache(String name) {
+        this.name = name;
+    }
+
+    @Nonnull
+    @Override
+    public <T> Optional<T> get(@Nonnull Object key, @Nonnull Argument<T> requiredType) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <T> T get(@Nonnull Object key, @Nonnull Argument<T> requiredType, @Nonnull Supplier<T> supplier) {
+        return supplier.get();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Nonnull
+    @Override
+    public <T> Optional<T> putIfAbsent(@Nonnull Object key, @Nonnull T value) {
+        return Optional.of(value);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void put(@Nonnull Object key, @Nonnull Object value) {
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void invalidate(@Nonnull Object key) {
+    }
+
+    @Override
+    public void invalidateAll() {
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Object getNativeCache() {
+        return this;
+    }
+}
