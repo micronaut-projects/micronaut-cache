1f2a11627f0e928ed3c38dd2057ffebeae34c7c6
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2019-11-29 07:53:03 +0100

    Publishing cache info statistics

--- cache-infinispan/src/main/java/io/micronaut/cache/infinispan/InfinispanCacheInfo.java
+++ cache-infinispan/src/main/java/io/micronaut/cache/infinispan/InfinispanCacheInfo.java
@@ -2,6 +2,9 @@ package io.micronaut.cache.infinispan;
 
 import io.micronaut.cache.CacheInfo;
 import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.impl.ClientStatistics;
+import org.infinispan.client.hotrod.jmx.RemoteCacheClientStatisticsMXBean;
 
 import javax.annotation.Nonnull;
 import java.util.LinkedHashMap;
@@ -24,10 +27,46 @@ public class InfinispanCacheInfo implements CacheInfo {
     @Nonnull
     @Override
     public Map<String, Object> get() {
+        Map<String, Object> cacheManager = getCacheManagerStatistics();
+        Map<String, Object> clientStatistics = getClientStatistics();
+        Map<String, String> serverStatistics = nativeCache.serverStatistics().getStatsMap();
+
+        Map<String, Object> infinispan = new LinkedHashMap<>(3);
+        infinispan.put("cacheManager", cacheManager);
+        infinispan.put("clientStatistics", clientStatistics);
+        infinispan.put("serverStatistics", serverStatistics);
+
         Map<String, Object> data = new LinkedHashMap<>(2);
         data.put("implementationClass", nativeCache.getClass().getName());
-        data.put("infinispan", nativeCache.clientStatistics().getAverageRemoteReadTime());
+        data.put("infinispan", infinispan);
         return data;
+    }
+
+    private Map<String, Object> getCacheManagerStatistics() {
+        Map<String, Object> cacheManager = new LinkedHashMap<>(3);
+        RemoteCacheManager remoteCacheManager = nativeCache.getRemoteCacheManager();
+        cacheManager.put("activeConnectionCount", remoteCacheManager.getActiveConnectionCount());
+        cacheManager.put("connectionCount", remoteCacheManager.getConnectionCount());
+        cacheManager.put("idleConnectionCount", remoteCacheManager.getIdleConnectionCount());
+        return cacheManager;
+    }
+
+    private Map<String, Object> getClientStatistics() {
+        Map<String, Object> clientStatistics = new LinkedHashMap<>(12);
+        RemoteCacheClientStatisticsMXBean clientStats = nativeCache.clientStatistics();
+        clientStatistics.put("averageRemoteReadTime", clientStats.getAverageRemoteReadTime());
+        clientStatistics.put("averageRemoteRemovesTime", clientStats.getAverageRemoteRemovesTime());
+        clientStatistics.put("averageRemoteStoreTime", clientStats.getAverageRemoteStoreTime());
+        clientStatistics.put("nearCacheHits", clientStats.getNearCacheHits());
+        clientStatistics.put("nearCacheInvalidations", clientStats.getNearCacheInvalidations());
+        clientStatistics.put("nearCacheMisses", clientStats.getNearCacheMisses());
+        clientStatistics.put("nearCacheSize", clientStats.getNearCacheSize());
+        clientStatistics.put("remoteHits", clientStats.getRemoteHits());
+        clientStatistics.put("remoteMisses", clientStats.getRemoteMisses());
+        clientStatistics.put("remoteRemoves", clientStats.getRemoteRemoves());
+        clientStatistics.put("remoteStores", clientStats.getRemoteStores());
+        clientStatistics.put("timeSinceReset", clientStats.getTimeSinceReset());
+        return clientStatistics;
     }
 
     @Nonnull
