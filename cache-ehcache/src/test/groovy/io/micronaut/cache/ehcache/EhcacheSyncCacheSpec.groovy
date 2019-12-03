2c6542dedad13906c968f0bd03d01ace0d97dcec
Davide Cocco <davide.cocco@oracle.com>
2019-12-03 08:53:23 +0100

    TCK implementation for Ehcache

--- cache-ehcache/src/test/groovy/io/micronaut/cache/ehcache/EhcacheSyncCacheSpec.groovy
+++ cache-ehcache/src/test/groovy/io/micronaut/cache/ehcache/EhcacheSyncCacheSpec.groovy
@@ -1,12 +1,24 @@
 package io.micronaut.cache.ehcache
 
+import io.micronaut.cache.SyncCache
 import io.micronaut.cache.tck.AbstractSyncCacheSpec
 import io.micronaut.context.ApplicationContext
+import org.ehcache.core.Ehcache
+import org.ehcache.impl.internal.store.heap.OnHeapStore
 
 class EhcacheSyncCacheSpec extends AbstractSyncCacheSpec {
 
     @Override
     ApplicationContext createApplicationContext() {
-        return ApplicationContext.run("ehcache.caches.counter.enabled": true)
+        return ApplicationContext.run(
+                "ehcache.caches.counter.enabled": true,
+                "ehcache.caches.counter2.enabled": true,
+                "ehcache.caches.test.heap.max-entries": 3
+        )
+    }
+
+    @Override
+    void flushCache(SyncCache syncCache) {
+        ((OnHeapStore) ((Ehcache)syncCache.nativeCache).store).enforceCapacity()
     }
 }
