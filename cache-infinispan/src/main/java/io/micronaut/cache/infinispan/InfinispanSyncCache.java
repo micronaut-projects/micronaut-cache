06e343bab9e60e77ff9676063dd22462a25d869a
Davide Cocco <davide.cocco@oracle.com>
2019-12-17 15:59:58 +0100

    Use AbstractMapBasedSyncCache

--- cache-infinispan/src/main/java/io/micronaut/cache/infinispan/InfinispanSyncCache.java
+++ cache-infinispan/src/main/java/io/micronaut/cache/infinispan/InfinispanSyncCache.java
@@ -15,20 +15,16 @@
  */
 package io.micronaut.cache.infinispan;
 
+import io.micronaut.cache.AbstractMapBasedSyncCache;
 import io.micronaut.cache.AsyncCache;
 import io.micronaut.cache.CacheInfo;
 import io.micronaut.cache.SyncCache;
 import io.micronaut.core.async.publisher.Publishers;
-import io.micronaut.core.convert.ConversionContext;
 import io.micronaut.core.convert.ConversionService;
-import io.micronaut.core.type.Argument;
-import io.micronaut.core.util.ArgumentUtils;
 import org.infinispan.client.hotrod.RemoteCache;
 import org.reactivestreams.Publisher;
 
 import javax.annotation.Nonnull;
-import java.util.Optional;
-import java.util.function.Supplier;
 
 /**
  * A {@link SyncCache} implementation based on Infinispan's {@link RemoteCache}.
@@ -36,92 +32,31 @@ import java.util.function.Supplier;
  * @author Álvaro Sánchez-Mariscal
  * @since 1.0.0
  */
-public class InfinispanSyncCache implements SyncCache<RemoteCache<Object, Object>> {
-
-    private final RemoteCache<Object, Object> nativeCache;
-    private final ConversionService<?> conversionService;
+public class InfinispanSyncCache extends AbstractMapBasedSyncCache<RemoteCache<Object, Object>> {
 
     /**
-     * @param nativeCache the Infinispan remote cache
      * @param conversionService the conversion service
+     * @param nativeCache       the native cache
      */
-    public InfinispanSyncCache(RemoteCache<Object, Object> nativeCache, ConversionService<?> conversionService) {
-        this.nativeCache = nativeCache;
-        this.conversionService = conversionService;
-    }
-
-    @Nonnull
-    @Override
-    public <T> Optional<T> get(@Nonnull Object key, @Nonnull Argument<T> requiredType) {
-        ArgumentUtils.requireNonNull("key", key);
-        Object value = nativeCache.get(key);
-        if (value != null) {
-            return conversionService.convert(value, ConversionContext.of(requiredType));
-        }
-        return Optional.empty();
-    }
-
-    @Override
-    public <T> T get(@Nonnull Object key, @Nonnull Argument<T> requiredType, @Nonnull Supplier<T> supplier) {
-        ArgumentUtils.requireNonNull("key", key);
-        Optional<T> existingValue = get(key, requiredType);
-        if (existingValue.isPresent()) {
-            return existingValue.get();
-        } else {
-            T value = supplier.get();
-            put(key, value);
-            return value;
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    @Nonnull
-    @Override
-    public <T> Optional<T> putIfAbsent(@Nonnull Object key, @Nonnull T value) {
-        ArgumentUtils.requireNonNull("key", key);
-        ArgumentUtils.requireNonNull("value", value);
-        final T v = (T) nativeCache.putIfAbsent(key, value);
-        final Class<T> aClass = (Class<T>) value.getClass();
-        return conversionService.convert(v, aClass);
-    }
-
-    @Override
-    public void put(@Nonnull Object key, @Nonnull Object value) {
-        ArgumentUtils.requireNonNull("key", key);
-        ArgumentUtils.requireNonNull("value", value);
-        nativeCache.put(key, value);
-    }
-
-    @Override
-    public void invalidate(@Nonnull Object key) {
-        ArgumentUtils.requireNonNull("key", key);
-        nativeCache.remove(key);
-    }
-
-    @Override
-    public void invalidateAll() {
-        nativeCache.clear();
+    public InfinispanSyncCache(ConversionService<?> conversionService, RemoteCache<Object, Object> nativeCache) {
+        super(conversionService, nativeCache);
     }
 
     @Override
     public String getName() {
-        return nativeCache.getName();
+        return getNativeCache().getName();
     }
 
-    @Override
-    public RemoteCache<Object, Object> getNativeCache() {
-        return nativeCache;
-    }
 
     @Nonnull
     @Override
     public AsyncCache<RemoteCache<Object, Object>> async() {
-        return new InfinispanAsyncCache(nativeCache, conversionService);
+        return new InfinispanAsyncCache(getNativeCache(), getConversionService());
     }
 
     @Override
     public Publisher<CacheInfo> getCacheInfo() {
-        return Publishers.just(new InfinispanCacheInfo(nativeCache));
+        return Publishers.just(new InfinispanCacheInfo(getNativeCache()));
     }
 
 }
