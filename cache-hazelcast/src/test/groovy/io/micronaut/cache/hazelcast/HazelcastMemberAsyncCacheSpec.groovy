d02f39f3ba701065d17e15611b30d2dac79ddb44
Davide Cocco <davide.cocco@oracle.com>
2020-01-24 10:01:04 +0100

    Attempt to fix tests

--- cache-hazelcast/src/test/groovy/io/micronaut/cache/hazelcast/HazelcastMemberAsyncCacheSpec.groovy
+++ cache-hazelcast/src/test/groovy/io/micronaut/cache/hazelcast/HazelcastMemberAsyncCacheSpec.groovy
@@ -46,7 +46,6 @@ class HazelcastMemberAsyncCacheSpec extends AbstractAsyncCacheSpec {
                     .setEvictionPolicy(EvictionPolicy.LRU)
                     .setName("test")
             event.getBean().addMapConfig(mapConfig)
-            event.getBean().setProperty("hazelcast.partition.count", "1")
             event.getBean()
         }
     }
