a42d9b2fe868f1656be677ef43444a125f7ce0a3
Cedric Champeau <cedric.champeau@gmail.com>
2025-12-03 11:03:51 +0100

    Build fixes

--- test-suite-caffeine-kotlin/src/test/kotlin/io/micronaut/cache/RemovalListenerImpl.kt
+++ test-suite-caffeine-kotlin/src/test/kotlin/io/micronaut/cache/RemovalListenerImpl.kt
@@ -6,7 +6,7 @@ import jakarta.inject.Singleton
 
 // tag::clazz[]
 @Singleton
-class RemovalListenerImpl internal constructor(private val handler: MyRemovalHandler) : RemovalListener<String?, Int?> {
+class RemovalListenerImpl internal constructor(private val handler: MyRemovalHandler) : RemovalListener<String, Int> {
 
     override fun onRemoval(key: String?, value: Int?, cause: RemovalCause) {
         handler.handle(key!!, value!!, cause)
