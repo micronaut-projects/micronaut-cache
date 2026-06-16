fb87ce54ec51ba88f3d004041c0bac68e101e43c
Davide Cocco <davide.cocco@oracle.com>
2026-06-16 11:13:14 +0200

    Updated since tags to 6.0.1

--- cache-core/src/main/java/io/micronaut/cache/interceptor/ParametersKey.java
+++ cache-core/src/main/java/io/micronaut/cache/interceptor/ParametersKey.java
@@ -50,7 +50,7 @@ public class ParametersKey implements Serializable {
 
     /**
      * @return A defensive copy of the parameters used to build this key
-     * @since 6.0.0
+     * @since 6.0.1
      */
     public Object[] getParameters() {
         return Arrays.copyOf(params, params.length);
